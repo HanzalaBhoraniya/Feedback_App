2fdc9b5e92369e545839a22ae57da9684daa7442
Hanzala Bhoraniya <mashkurabhoraniya5@gmail.com>
2026-07-24 10:52:53 +0530

    Added SSLconfig for neon DB

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -4,15 +4,21 @@ import dotenv from "dotenv";
 dotenv.config();
 const { Pool } = pkg;
 
+const poolConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }, // Required for Neon!
+    }
+  : {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+    };
 
 // creating new connection. means this is just a set-up we are saying node.js that if we say to connect use this credentials and connect with postgres
-const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-})
+const pool = new Pool(poolConfig)
 
 // adding a sensor that whenever my node.js connects with the postgres this function executes.
 pool.on("connect", () => {
